662b54a90ee52574757e455683170781d9f1dd67
Nicholas2208 <nikolaj.belyakov.84@gmail>
2023-06-03 10:59:39 +0300

    Add Product REST service and TestContainer dependencies

---
+++ product-service/src/main/java/com/nwhite/productservice/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.nwhite.productservice.repository;
+
+import com.nwhite.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
